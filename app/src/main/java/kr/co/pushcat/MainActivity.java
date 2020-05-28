8b8741b6a6cab25aa637e9d6768a107d8f50777d
YoonDo <dhdbseh5oe@gmail.com>
2020-05-28 18:49:03 +0900

    new_singup

--- app/src/main/java/kr/co/pushcat/MainActivity.java
+++ app/src/main/java/kr/co/pushcat/MainActivity.java
@@ -3,12 +3,25 @@ package kr.co.pushcat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_id;
+    Button btn_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        et_id = findViewById(R.id.et_id);
+        btn_test = findViewById(R.id,btn_test);
+
+        btn_test.setOnClickListener((new.View.onClickListener));
+
+
     }
 }
